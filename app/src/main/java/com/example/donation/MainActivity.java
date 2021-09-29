a7c9e8092bc5d989fd2ed405cca842b08b93f6bb
itachi176 <lhoang17062000@gmail.com>
2021-09-29 22:48:50 +0700

    bai 2

--- app/src/main/java/com/example/donation/MainActivity.java
+++ app/src/main/java/com/example/donation/MainActivity.java
@@ -37,4 +37,28 @@ public class MainActivity extends AppCompatActivity {
             Log.v("Donate", "Really got the donate button");
         }
     }
+
+    public void donateButtonPressed(View view){
+        Log.v("Donate", "Donate Pressed");
+    }
+
+
+//    public Boolean onCreateOptionMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_donate, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//    // Handle action bar item clicks here. The action bar will
+//    // automatically handle clicks on the Home/Up button, so long
+//    // as you specify a parent activity in AndroidManifest.xml.
+//            int id = item.getItemId();
+//    //noinspection SimplifiableIfStatement
+//            if (id == R.id.action_settings) {
+//                return true;
+//            }
+//            return super.onOptionsItemSelected(item);
+//    }
 }
